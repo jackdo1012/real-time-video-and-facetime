ab801c1028e9e3c4207230dc7490d9f66b645054
PhamCong01 <phamvancong300701@gmail.com>
2022-03-18 19:56:28 +0700

    check oldUser in db

--- server/src/controllers/AuthControllers.js
+++ server/src/controllers/AuthControllers.js
@@ -1,27 +1,28 @@
-const {getUserData} = require('../services/auth.service')
-const authUser = require('../models/auth')
+const { getUserData } = require("../services/auth.service");
+const authUser = require("../models/auth");
 class AuthControllers {
-    // [POST]/login
-    login = async(req, res) => {
-        try {
-            const accessToken = req.headers.authorization.split(" ")[1]
-            const userData = await getUserData(accessToken)
-            const user = new authUser({
-                given_name: userData.given_name,
-                nickname: userData.nickname,
-                name: userData.name,
-                picture: userData.picture,
-                email: userData.email,
-            })
-            if(!user) {
-                await user.save()
-            }
-            res.json(user)
-            
-        } catch (error) {
-            console.log(error)
-        }
+  // [POST]/login
+  login = async (req, res) => {
+    try {
+      const accessToken = req.headers.authorization.split(" ")[1];
+      const userData = await getUserData(accessToken);
+      const oldUser = await authUser.find({ email: userData.email });
+      if (!oldUser) {
+        const user = new authUser({
+          given_name: userData.given_name,
+          nickname: userData.nickname,
+          name: userData.name,
+          picture: userData.picture,
+          email: userData.email,
+        });
+        await user.save();
+
+        res.json(user);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
-module.exports = new AuthControllers()
+module.exports = new AuthControllers();
